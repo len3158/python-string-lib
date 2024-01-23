f0a282f1da738a45c043b987cb27a00c95d4065f
Lenny Siemeni <len3158@gmail.com>
2024-01-23 18:05:37 -0500

    unit tests run command

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # python-string-lib
 A simple pyhton string library
+
+To run the tests, run the command
+
+``
+python3 -m unittest .\tests\unit_tests.py
+``
--- tests/unit_tests.py
+++ tests/unit_tests.py
@@ -1,39 +1,45 @@
+import unittest
 from src.simple_string_lib import compare_versions
 
 
-def test_string():
-    assert compare_versions("1.2", "1.1") == 1
-    assert compare_versions("1.1", "1.2") == -1
-    assert compare_versions("1.2", "1.2") == 0
-    assert compare_versions("1.2.0", "1.2") == 0
-    assert compare_versions("1.2.1", "1.2") == 1
-    assert compare_versions("1.2", "1.2.1") == -1
-    assert compare_versions("1.2.9", "1.3.0") == -1
-    assert compare_versions("2.0", "1.9999.9999") == 1
-    assert compare_versions("1.0.0.0", "1") == 0
-    assert compare_versions("0.9", "1.0") == -1
-    # Test cases for invalid inputs
-    try:
-        compare_versions("1.2a", "1.2")
-        assert False, "Expected ValueError"
-    except ValueError:
-        pass
+class Testing(unittest.TestCase):
+    def test_string(self):
+        assert compare_versions("1.2", "1.1") == 1
+        assert compare_versions("1.1", "1.2") == -1
+        assert compare_versions("1.2", "1.2") == 0
+        assert compare_versions("1.2.0", "1.2") == 0
+        assert compare_versions("1.2.1", "1.2") == 1
+        assert compare_versions("1.2", "1.2.1") == -1
+        assert compare_versions("1.2.9", "1.3.0") == -1
+        assert compare_versions("2.0", "1.9999.9999") == 1
+        assert compare_versions("1.0.0.0", "1") == 0
+        assert compare_versions("0.9", "1.0") == -1
+        # Test cases for invalid inputs
+        try:
+            compare_versions("1.2a", "1.2")
+            assert False, "Expected ValueError"
+        except ValueError:
+            pass
 
-    try:
-        compare_versions(1.2, "1.2")
-        assert False, "Expected TypeError"
-    except TypeError:
-        pass
+        try:
+            compare_versions(1.2, "1.2")
+            assert False, "Expected TypeError"
+        except TypeError:
+            pass
 
-    try:
-        compare_versions("1..2", "1.2")
-        assert False, "Expected ValueError"
-    except ValueError:
-        pass
+        try:
+            compare_versions("1..2", "1.2")
+            assert False, "Expected ValueError"
+        except ValueError:
+            pass
 
-    try:
-        compare_versions("1.2", None)
-        assert False, "Expected TypeError"
-    except TypeError:
-        pass
-    print("All test cases passed")
+        try:
+            compare_versions("1.2", None)
+            assert False, "Expected TypeError"
+        except TypeError:
+            pass
+        print("All test cases passed")
+
+
+if __name__ == '__main__':
+    unittest.main()
